3c7c6eb030ebd738b3ed33996348073a962f1c2a
Ahmed <sdmudakir@gmail.com>
2025-01-09 21:48:56 +0300

    modified wrong changes

--- app.py
+++ app.py
@@ -4,8 +4,5 @@ client = boto3.client('ec2')
 response = client.run_instances(
     ImageId='ami-0453ec754f44f9a4a',
     InstanceType='t2.nano',
-    KeyName = 'devops',
-
-    MaxCount=1,
-    MinCount=1,
+    KeyName = 
 )
