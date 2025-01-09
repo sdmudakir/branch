9ac7b4f8d4061468a96a4f5ca517dc16d04709d3
Ahmed <sdmudakir@gmail.com>
2025-01-09 22:09:57 +0300

    modifed by aws

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import boto3
 client = boto3.client('ec2')
 response = client.run_instances(
             ImageId='ami-0453ec754f44f9a4a',
-                InstanceType='t2',
+                InstanceType='t2.medium',
                     KeyName = 'devops',
 
                         MaxCount=1,
